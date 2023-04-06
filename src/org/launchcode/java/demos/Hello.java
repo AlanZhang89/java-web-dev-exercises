6e1539227720d217e352f767a165c723c76d75d8
Zhengwu Zhang <zhengwu225@gmail.com>
2023-04-06 14:36:22 -0500

    add addGrade and getGradeLevel method

--- src/org/launchcode/java/demos/Hello.java
+++ src/org/launchcode/java/demos/Hello.java
@@ -1,10 +1,22 @@
 package org.launchcode.java.demos;
 
+import java.util.HashMap;
+
 /**
  * From "Java Web Development"
  */
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        HashMap<String, Integer> moons = new HashMap<>();
+        moons.put("Mercury", 0);
+        moons.put("Venus", 0);
+        moons.put("Earth", 1);
+        moons.put("Mars", 2);
+        moons.put("Jupiter", 79);
+        moons.put("Saturn", 82);
+        moons.put("Uranus", 27);
+        moons.put("Neptune", 14);
+        System.out.println(moons.keySet());
     }
 }
